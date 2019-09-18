19403e642bac6a3daaebf630899d64a3926708d7
Nikita Glazkov <zitrnik@gmail.com>
2019-09-18 16:33:48 +0300

    Clear project

---
+++ src/components/Entrance.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <form action @submit.prevent>
+      <label for="name">Ваше имя</label>
+      <br />
+      <input type="text" name="name" id="name" />
+      <br />
+      <button>Войти</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Entrance",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Main</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import Entrance from "@/components/Entrance.vue";
+
+export default {
+  name: "Main",
+  components: {}
+};
+</script>
