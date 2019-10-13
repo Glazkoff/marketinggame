75a3d9cacabfde07f2d769512be3842e64972663
Nikita Glazkov <zitrnik@gmail.com>
2019-10-14 00:30:12 +0300

    Some server functionality

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
-const server = app.listen(3001, function () {
+// server.listen(process.env.PORT || 3000);
+const server = app.listen(process.env.PORT || 3001, function () {
   console.log("server running on port 3001");
 });
 
