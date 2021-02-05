2ecc43e6bc21815a6d38f0151be22c889e656e83
Nikita Glazkov <zitrnik@gmail.com>
2021-02-05 10:11:08 +0300

    DEV: add Card  model

---
+++ server/migrations/20210205070101-create-card.js
@@ -0,0 +1,52 @@
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Cards", {
+      card_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      text: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      },
+      coefs: {
+        type: Sequelize.JSONB,
+        defaultValue: []
+      },
+      templateText: {
+        type: Sequelize.TEXT
+      },
+      cost: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      duration: {
+        type: Sequelize.INTEGER
+      },
+      data_change: {
+        type: Sequelize.JSONB,
+        allowNull: false
+      },
+      oneOff: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Cards");
+  }
+};
---
+++ server/models/card.js
@@ -0,0 +1,57 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Card extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Card.init(
+    {
+      card_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      text: {
+        type: DataTypes.TEXT,
+        allowNull: false
+      },
+      coefs: {
+        type: DataTypes.JSONB,
+        defaultValue: []
+      },
+      templateText: {
+        type: DataTypes.TEXT
+      },
+      cost: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      },
+      duration: {
+        type: DataTypes.INTEGER
+      },
+      data_change: {
+        type: DataTypes.JSONB,
+        allowNull: false
+      },
+      oneOff: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: false
+      }
+    },
+    {
+      sequelize,
+      modelName: "Card"
+    }
+  );
+  return Card;
+};
