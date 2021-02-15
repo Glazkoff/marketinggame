6dc2afecde4bf09386b211507796f639d0607c35
Fiasko <snakeice2000@mail.ru>
2021-02-15 20:47:00 +0300

    FIX: check dublicate kick user
    check dublicate user in kick array when owner is kicking gamer

--- server/socket_events/kick_user.js
+++ server/socket_events/kick_user.js
@@ -45,7 +45,6 @@ module.exports = function(socket, io, db) {
           }
         ]
       });
-
       // Устанавливаем, что пользователь подключён к комнате
       await db.UserInRoom.update(
         {
@@ -60,11 +59,14 @@ module.exports = function(socket, io, db) {
       );
 
       // Добавляем кикнутого пользователя
-      if (room.kicked_participants_id !== null) {
-        room.kicked_participants_id.push(data.gamerId);
-      } else {
+      let KickList = room.kicked_participants_id
+      if (KickList !== null)
+        if (!KickList.includes(data.gamerId)) {
+          room.kicked_participants_id.push(data.gamerId);
+        } 
+      else
         room.kicked_participants_id = [data.gamerId];
-      }
+
       await db.Room.update(
         {
           kicked_participants_id: room.kicked_participants_id
