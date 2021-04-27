89aa7b1231a6b0581091f8fb6eccd33fff0307f1
Renelvah <sibiriennacht@yandex.ru>
2021-04-27 22:10:11 +0300

    FIX: fix design adaptability for rating columns

--- src/components/Choose/Rating.vue
+++ src/components/Choose/Rating.vue
@@ -3,22 +3,24 @@
     <h3 class="mb-1">Рейтинг игроков</h3>
     <div class="">
       <div class="d-flex mt-1 pt-2 pb-2 text-info font-weight-bolder text-center align-items-center">
-        <span class="col-6 col-sm-3">Место</span>
-        <span class="col-6 col-sm-3">Игрок</span>
-        <span class="col-2 d-none d-sm-block"><img width="32px" src="@/assets/1.svg" alt="First" title="Первые места"></span>
+        <span class="col-6 col-sm-3 col-md-2">Место</span>
+        <span class="col-6 col-sm-3 col-md-4">Игрок</span>
+        <span class="col-2 d-none d-sm-block"><img width="32px" src="@/assets/1.svg" alt="First"
+                                                   title="Первые места"></span>
         <span class="col-2 d-none d-sm-block"><img width="32px" src="@/assets/2.svg" alt="Second" title="Вторые места"></span>
-        <span class="col-2 d-none d-sm-block"><img width="32px" src="@/assets/3.svg" alt="Third" title="Третьи места"></span>
+        <span class="col-2 d-none d-sm-block"><img width="32px" src="@/assets/3.svg" alt="Third"
+                                                   title="Третьи места"></span>
       </div>
       <div class="d-flex mt-1 pt-2 pb-2 text-center align-items-center"
            v-for="(user, index) in usersRating"
            :key="user.user_id"
            :class="{'border-top mt-2 mt-2': index === 3}"
       >
-        <span class="col-6 col-sm-3 font-weight-bolder"
+        <span class="col-6 col-sm-3 col-md-2 font-weight-bolder"
               :style="{color: placeColor[index]}"
         >
           {{ user.id + 1 }}</span>
-        <span class="col-6 col-sm-3">{{ user.name }}</span>
+        <span class="col-6 col-sm-3 col-md-4">{{ user.name }}</span>
         <span class="col-2 d-none d-sm-block"
               v-for="i in 3"
               :key="i"
