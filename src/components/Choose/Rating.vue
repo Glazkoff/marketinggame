ca6ae2a841fe1ac11c190d8445f41653e64c4f6f
Renelvah <sibiriennacht@yandex.ru>
2021-04-30 00:34:55 +0300

    DEV: change medals icons

--- src/components/Choose/Rating.vue
+++ src/components/Choose/Rating.vue
@@ -4,10 +4,10 @@
       <div class="d-flex mt-1 pt-2 pb-2 text-info font-weight-bolder text-center align-items-center">
         <span class="col-6 col-sm-3 col-md-3 col-lg-3 col-xl-2">№</span>
         <span class="col-6 col-sm-3 col-md-3 col-lg-3 col-xl-4">Игрок</span>
-        <span class="col-2 places"><img width="32px" src="@/assets/1.svg" alt="First"
+        <span class="col-2 places"><img width="32px" src="@/assets/medals/1.svg" alt="First"
                                                    title="Первые места"></span>
-        <span class="col-2 places"><img width="32px" src="@/assets/2.svg" alt="Second" title="Вторые места"></span>
-        <span class="col-2 places"><img width="32px" src="@/assets/3.svg" alt="Third"
+        <span class="col-2 places"><img width="32px" src="@/assets/medals/2.svg" alt="Second" title="Вторые места"></span>
+        <span class="col-2 places"><img width="32px" src="@/assets/medals/3.svg" alt="Third"
                                                    title="Третьи места"></span>
       </div>
       <div class="d-flex mt-1 pt-1 pb-1 text-center align-items-center border-top"
@@ -18,7 +18,11 @@
                 :style="{color: placeColor[index]}"
           >
             {{ user.id + 1 }}</span>
-          <span class="col-6 col-sm-3 col-md-3 col-lg-3 col-xl-4">{{ user.name }}</span>
+          <span class="col-6 col-sm-3 col-md-3 col-lg-3 col-xl-4">
+            {{ user.name }}
+            <small v-show="userId === user.user_id"
+                   class="text-secondary">(вы)</small>
+          </span>
           <span class="col-2 places"
                 v-for="i in 3"
                 :key="i"
@@ -35,6 +39,7 @@ export default {
   data() {
     return {
       usersRating: [],
+      userId: -1,
       placeColor: ['#e49034', '#639fa7', '#dc314b']
     }
   },
@@ -48,6 +53,7 @@ export default {
           this.usersRating = (res.data).slice(0, 10)
           let decode = jwt.decode(this.$store.state.token);
           if (decode) {
+            this.userId = decode.id
             res.data.forEach(data => {
               // Если совпадают id и пользователь уже не находится в тройке победителей
               if (data.user_id === decode.id && this.usersRating.findIndex(user => user.user_id === data.user_id) === -1)
