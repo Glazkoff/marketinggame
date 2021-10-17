8d04f22f335c3a2fb238785f008181302a2a8e5d
Nikita Glazkov <zitrnik@gmail.com>
2021-10-17 23:57:32 +0300

    DEV: settings for DB

--- server/db.config.js
+++ server/db.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   HOST: "37.140.192.53",
-  USER: "u1503522_default",
-  PASSWORD: "o5KnS778sGxP9NoG",
-  DB: "u1503522_default",
+  USER: "u1503522_ngl",
+  PASSWORD: "u1503522_ngl",
+  DB: "u1503522_ngl"
 };
